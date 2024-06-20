7bbb0b0ae3bf2b1ae0fb03fcd07aa7281d2c2c87
wendellast <wendelalves01a@hotmail.com>
2024-06-19 23:35:01 -0300

    router created

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/wendellast/Orange/src/controler/routes"
 )
 
 func main() {
@@ -14,5 +14,13 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Print(os.Getenv("TEST"))
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
 }
